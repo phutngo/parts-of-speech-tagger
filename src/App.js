d5a74ec90428881115f964b6eb94b8ce79f3aa5e
Phu Ngo <ptn123@gmail.com>
2021-08-03 22:05:18 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import CSVReader2 from "./components/CSVReader2";
+import CSVDownloader1 from "./components/CSVDownloader";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CSVReader2 />
+      <CSVDownloader1/>
+    </>
   );
 }
 
